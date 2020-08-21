2375923a61e81a5baf345d451562c3561b0d46f6
shandysetiawan <shandysetiawanliu@gmail.com>
2020-08-21 14:06:34 +0700

    add image component

---
+++ src/components/image/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Image = styled.img`
+width: 100%;
+height: 200px;
+object-fit: cover;
+border-top-left-radius: inherit;
+border-top-right-radius: inherit;
+`;
