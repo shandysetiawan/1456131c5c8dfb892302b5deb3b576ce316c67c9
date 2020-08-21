0445e4122ea582ac9f5c4a65ae39878ee0d564ad
shandysetiawan <shandysetiawanliu@gmail.com>
2020-08-21 14:01:28 +0700

    add card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { Card } from './components';
 
 function App() {
   return (
     <ThemeProvider theme={{}}>
+      <Card>Kulina Test</Card>
     </ThemeProvider>
   );
 }
---
+++ src/components/card/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+    box-shadow: 0 8px 10px 0 rgba(10, 31, 68, 0.1);
+    border-radius: 8px;
+`;
