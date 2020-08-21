dfe557c41aeefb5c392fed2ab12a8a969a4facc5
shandysetiawan <shandysetiawanliu@gmail.com>
2020-08-21 14:07:00 +0700

    integrate menu in card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { Card } from './components';
+import { Card, Image } from './components';
+import menus from './data/menu';
 
 function App() {
   return (
     <ThemeProvider theme={{}}>
-      <Card>Kulina Test</Card>
-    </ThemeProvider>
+      {menus.map(menu => <Card>
+        <Image src={menu.image} />
+        <div>rating</div>
+        <h2>{menu.title}</h2>
+        <div>{menu.cook_by} &middot; {menu.category}</div>
+        <div>{menu.price}</div>
+      </Card>)
+      }
+    </ThemeProvider >
   );
 }
 
