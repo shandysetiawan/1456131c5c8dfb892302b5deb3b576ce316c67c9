5bf64ee99e00d3f73f3830b3c391afe06b48cb86
shandysetiawan <shandysetiawanliu@gmail.com>
2020-09-01 21:32:58 +0700

    deploy edit

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -15,16 +15,23 @@ const ModalContainer = styled.div`
 
 const ModalContent = styled.div`
   position: absolute;
+  width: 370px;
   height: 80vh;
   bottom: 0;
   background-color: #fff;
   border-top-left-radius: 8px;
   border-top-right-radius: 8px;
+  font-size: 12px;
+  margin-left: 37%;
 `;
 
 const ModalChildren = styled.div`
   margin-top: 32px;
   padding: 0 12px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  line-height: 1.5em;
 `;
 
 export const Modal = (props) =>
